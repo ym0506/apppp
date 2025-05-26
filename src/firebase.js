2c7ab2e32eb560be170b940e38290db90bd0b81c
ym0506 <atat9828@naver.com>
2025-05-27 01:56:22 +0900

    add

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Firebase 구성 정보
 // 실제 값은 .env 파일 또는 환경 변수로 관리하는 것이 좋습니다
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCPE45MtrHjzX4Mrj7OScNuyE_JJVOx6Qg",
+  authDomain: "login-baa7f.firebaseapp.com",
+  projectId: "login-baa7f",
+  storageBucket: "login-baa7f.firebasestorage.app",
+  messagingSenderId: "296899354710",
+  appId: "1:296899354710:web:fcf0d584b294b8a9505bf7",
+  measurementId: "G-SNZFLHVVSX"
 };
 
 // Firebase 초기화
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 // Auth 및 Firestore 인스턴스 생성
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 
 export default app; 
